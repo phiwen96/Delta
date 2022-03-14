e871f261bf7330d3f35ab4fca2bc783b5abdef6b
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2022-03-14 11:47:43 +0100

    a

--- modules/Char.cpp
+++ modules/Char.cpp
@@ -10,7 +10,7 @@ export
 		or Convertible_to <T, unsigned char>
 		or Convertible_to <T, char16_t>
 		or Convertible_to <T, char32_t>
-		or Convertible_to <T, wchar_t>;
+		or Convertible_to <T, wchar_t>; 
 }
 
 
