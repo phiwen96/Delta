4e6660c8a884f2034cfa1597b08b3c0cfb0132ca
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2022-03-20 12:46:50 +0100

    Update TLS.cpp

--- TLS.cpp
+++ TLS.cpp
@@ -1,7 +1,8 @@
 // Transport Layer Security
 
 module;
-#include "time.h"
+#include <time.h>
+#include <stdio.h>
 export module Delta.Net.TLS;
 
 struct protocol_version
@@ -25,9 +26,18 @@ struct random
 
 struct client_hello
 {
+    client_hello (protocol_version const& client)
+    {
+        
+    }
     protocol_version client_version;
     random randoom;
-    
+    unsigned char session_id_length;
+    unsigned char* session_id;
+    unsigned short cipher_suites_length;
+    unsigned short* cipher_suites;
+    unsigned char compression_methods_length;
+    unsigned char* compression_methods;
 };
 
 constexpr auto master_secret_length = 48;
@@ -47,15 +57,33 @@ struct tls
 {  
     tls (int sockid)
     {
+        memset (master_secret, '\0', master_secret_length);
+        memset (client_random, '\0', random_length);
+        memset (server_random, '\0', random_length);
+    }
+    
+private:
+    auto send_client_hello ()
+    {
+        auto package = client_hello {};
         
     }
-private:
+    
+    master_secret_type master_secret;
+    random_type client_random;
+    random_type server_random;
+    
     protected_params pending_send_params;
     protected_params pending_recv_params;
     protected_params active_send_params;
     protected_params active_recv_params;
-
     
+  // RSA public key, if supplied
+  // public_key_info       server_public_key;
+  // DH public key, if supplied (either in a certificate or ephemerally)
+  // Note that a server can legitimately have an RSA key for signing and
+  // a DH key for key exchange (e.g. DHE_RSA)
+  // dh_key                server_dh_key
 };
 
 
