f16e0d0f8a1bc39ad7ad8dc5bed058c32fd6efa8
philip wenkel <philip.sve@hotmail.com>
2022-11-24 11:48:38 +0100

    a

--- Nej.cpp
+++ Nej.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <coroutine>
 #include <utility>
+#include <vulkan/vulkan_core.h>
+#include <GLFW/glfw3.h>
 import Vulkan;
 // import Standard;
 
@@ -12,11 +14,30 @@ struct vRectangle {
 
 };
 
+struct make_window {
+	int width, height;
+	char const* title;
+};
+
+struct is_clicked {
+	GLFWwindow* handle;
+};
+
+struct is_closed {
+	GLFWwindow* handle;
+};
+
 struct something {
 	struct promise_type {
 		std::coroutine_handle <> continuation;
+		// GLFWwindow* window;
 		// char* txt;
 		auto get_return_object () -> something {
+			glfwInit ();
+			glfwWindowHint (GLFW_CLIENT_API, GLFW_NO_API);
+			// window = glfwCreateWindow (640, 480, "window 1", nullptr, nullptr);
+			// glfwSetWindowUserPointer (handle, this);
+			// glfwSetWindowCloseCallback (handle, window_close_callback);
 			return something {*this};
 		}
 		auto initial_suspend () -> std::suspend_never {
@@ -45,30 +66,95 @@ struct something {
 		auto return_void () noexcept -> void {
 
 		}
-		// auto await_transform (vWindowEvent) -> auto {
-		// 	struct awaiter {
-		// 		auto await_ready () noexcept -> bool {
+		auto await_transform (make_window&& m) -> auto {
+			
+			struct awaiter {
+				auto await_ready () noexcept -> bool {
 					
-		// 			return false;
-		// 		}
-		// 		auto await_suspend (std::coroutine_handle <promise_type> h) noexcept -> std::coroutine_handle <> {
-		// 			// return h.promise().continuation;
-		// 			// std::cout << "future::final_suspend::await_suspend" << std::endl;
-		// 			return h.promise().continuation ? h.promise().continuation : std::noop_coroutine ();
-		// 		}
-		// 		auto await_resume () noexcept -> void {
-		// 			// std::cout << "future::final_suspend::await_resume" << std::endl;
-		// 			// std::cout << "final_suspend::await_resume" << std::endl;
-		// 		}
-		// 	};
-		// }
+					return true;
+				}
+				auto await_suspend (std::coroutine_handle <promise_type> h) noexcept -> std::coroutine_handle <> {
+					// return h.promise().continuation;
+					// std::cout << "future::final_suspend::await_suspend" << std::endl;
+					return h.promise().continuation ? h.promise().continuation : std::noop_coroutine ();
+				}
+				auto await_resume () noexcept -> auto {
+					// std::cout << "future::final_suspend::await_resume" << std::endl;
+					// std::cout << "final_suspend::await_resume" << std::endl;
+					return glfwCreateWindow (640, 480, "window 1", nullptr, nullptr);
+				}
+			};
+			
+			return awaiter {};
+		}
+		static void mouse_button_callback(GLFWwindow* w, int button, int action, int mods)
+		{
+			// if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+			if (action == GLFW_RELEASE) {
+				glfwSetMouseButtonCallback (w, nullptr);
+				std::coroutine_handle <promise_type>::from_address (glfwGetWindowUserPointer (w)).resume ();
+			}
+				
+		}
+		static void window_close_callback (GLFWwindow* w)
+		{
+			glfwSetWindowCloseCallback (w, nullptr);
+			std::coroutine_handle <promise_type>::from_address (glfwGetWindowUserPointer (w)).resume ();
+				// glfwSetWindowShouldClose(window, GLFW_FALSE);
+		}
+		auto await_transform (is_clicked&& m) -> auto {
+			glfwSetMouseButtonCallback (m.handle, mouse_button_callback);
+			glfwSetWindowUserPointer (m.handle, std::coroutine_handle <promise_type>::from_promise (*this).address());
+			struct awaiter {
+				auto await_ready () noexcept -> bool {
+					
+					return false;
+				}
+				auto await_suspend (std::coroutine_handle <promise_type> h) noexcept -> std::coroutine_handle <> {
+					// return h.promise().continuation;
+					// std::cout << "future::final_suspend::await_suspend" << std::endl;
+					return h.promise().continuation ? h.promise().continuation : std::noop_coroutine ();
+				}
+				auto await_resume () noexcept -> void {
+					// std::cout << "future::final_suspend::await_resume" << std::endl;
+					// std::cout << "final_suspend::await_resume" << std::endl;
+					// return glfwCreateWindow (640, 480, "window 1", nullptr, nullptr);
+				}
+			};
+			
+			return awaiter {};
+		}
+		auto await_transform (is_closed&& m) -> auto {
+			glfwSetWindowCloseCallback (m.handle, window_close_callback);
+			glfwSetWindowUserPointer (m.handle, std::coroutine_handle <promise_type>::from_promise (*this).address());
+			struct awaiter {
+				auto await_ready () noexcept -> bool {
+					
+					return false;
+				}
+				auto await_suspend (std::coroutine_handle <promise_type> h) noexcept -> std::coroutine_handle <> {
+					// return h.promise().continuation;
+					// std::cout << "future::final_suspend::await_suspend" << std::endl;
+					return h.promise().continuation ? h.promise().continuation : std::noop_coroutine ();
+				}
+				auto await_resume () noexcept -> void {
+					// std::cout << "future::final_suspend::await_resume" << std::endl;
+					// std::cout << "final_suspend::await_resume" << std::endl;
+					// return glfwCreateWindow (640, 480, "window 1", nullptr, nullptr);
+				}
+			};
+			
+			return awaiter {};
+		}
 	};
 
 	auto open () const -> bool {
-		return handle.done();
+		return not handle.done();
+		// return not glfwWindowShouldClose (handle.promise().window);
 	}
 
 	auto draw () {
+		glfwPollEvents ();
 		
 	}
 
@@ -80,13 +166,71 @@ private:
 	std::coroutine_handle <promise_type> handle;
 };
 
+struct vWindow {
+	vWindow (auto width, auto height, auto title) : handle {glfwCreateWindow (width, height, title, nullptr, nullptr)} {
+
+	}
+
+	struct vWindowEvent {
+		vWindowEvent (GLFWwindow* h) : handle {h} {}
+		auto closed () {
+			struct awaitable {
+					auto await_ready () noexcept -> bool {
+						
+						return false;
+					}
+					// in this case, the await_coro is wanna get notified, so we should resume/return its continuation
+					auto await_suspend (std::coroutine_handle <something::promise_type> awaiting_coro) noexcept -> std::coroutine_handle <> {
+						// return h.promise().continuation;
+						// std::cout << "future::final_suspend::await_suspend" << std::endl;
+						return awaiting_coro.promise().continuation ? awaiting_coro.promise().continuation : std::noop_coroutine ();
+						// return h;
+						// return awaiting_coro;
+					}
+					auto await_resume () noexcept -> void {
+						// std::cout << "future::final_suspend::await_resume" << std::endl;
+						// std::cout << "final_suspend::await_resume" << std::endl;
+					}
+				};
+				return awaitable {};
+		}
+
+	private:
+		GLFWwindow* handle;
+	};
+
+	auto is () -> vWindowEvent {
+		return {handle};
+	}
+private:
+	GLFWwindow* handle;
+};
+
+struct vApp {
+
+};
+
+
+
 auto run () -> something {
-	auto window = vWindow {640, 480, "window 1"};
-	co_await window.is().closed();
+	std::cout << "run ..." << std::endl;
+	auto window = glfwCreateWindow (640, 480, "window 1", nullptr, nullptr);
+	// auto button = 
+	// co_await is_clicked {make_rectangle {window, 10, 10}};
+	co_await is_clicked {window};
+	co_await is_closed {window};
+
+	// co_await is_clicked {window};
+	// co_await is_clicked {window};
+	// co_await is_clicked {window};
+	
+	
+	// co_await mainWindow.is().closed();
 	// auto button = window.make_rectangle(10, 10);
 	// co_await button.is().hovered();
-	std::cout << "button hovered!" << std::endl;
-	std::cout << "exiting app!" << std::endl;
+	// std::cout << "button hovered!" << std::endl;
+	// std::cout << "exiting app!" << std::endl;
+	std::cout << "... run" << std::endl;
 	co_return;
 }
 
@@ -98,15 +242,19 @@ auto run () -> something {
 
 
 
+
+
 auto main (int argc, char** argv) -> int {
 	// auto instance = vInstance {{"VK_LAYER_KHRONOS_validation"}, {"VK_KHR_surface"}};
 	// auto physicalDevice = vPhysicalDevice {instance};
-
-
+	
+	std::cout << "main ..." << std::endl;
+	
 	auto app = run ();
 
 	while (app.open ())
 	{
+		// std::cout << "hello world" << std::endl;
 		app.draw ();
 	}
 	
@@ -115,8 +263,9 @@ auto main (int argc, char** argv) -> int {
 		
 	// 	physicalDevice};
 	// auto instance = vInstance {{"VK_LAYER_KHRONOS_validation"}, {"VK_KHR_surface"}};
-	std::cout << "hello world" << std::endl;
+	
 	// std::vector <int> baj;
 	// VkInstance inst;
+	std::cout << "... main" << std::endl;
 	return 0;
 }
